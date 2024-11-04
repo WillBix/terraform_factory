efe6086983ab4757c35635b7ba6f860251343267
WillBix <william.caires@bix-tech.com>
2024-11-04 00:40:07 -0300

    exec

--- task1/main.tf
+++ task1/main.tf
@@ -2,73 +2,63 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Security Group para permitir tráfego de entrada
 resource "aws_security_group" "airflow_sg" {
-  name        = "airflow_security_group"
-  description = "Permitir SSH e acesso HTTP para o Airflow"
+  name        = "terraform_ingress" 
+  description = "Grupo de seguranca para permitir trafego de entrada"
 
   ingress {
-    description = "Inbound Rule"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Acesso SSH aberto
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir SSH de qualquer IP
   }
 
   ingress {
-    description = "Inbound Rule"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Porta padrão do Airflow
-  }
-
-  egress {
-    description = "Outbound Rule"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir acesso à porta 8080 (Airflow)
   }
 }
 
-resource "aws_instance" "airflow_instance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  security_groups = [aws_security_group.airflow_sg.name]
+# Security Group para permitir tráfego de saída
+resource "aws_security_group" "allow_all_traffic_egress" {
+  name        = "terraform_egress" 
+  description = "Grupo de seguranca para permitir todo o trafego de saida"
 
-  # Lê o conteúdo do arquivo init-airflow.sh e o coloca no user_data
-  user_data = file("scripts/init-airflow.sh")
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir todo o tráfego de saída
+  }
+}
+
+# Carregar o script de inicialização
+data "template_file" "init" {
+  template = file("scripts_bash/init-airflow.sh")
+}
+
+# Instância EC2
+resource "aws_instance" "airflow_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  security_groups        = [aws_security_group.airflow_sg.name]
+  
+  vpc_security_group_ids = [aws_security_group.allow_all_traffic_egress.id] 
+
+  associate_public_ip_address = true
+  user_data = data.template_file.init.rendered
 
   tags = {
     Name = "Airflow-EC2-Instance"
-  }
+  } 
+}
 
-  # Provisioners para configurar o Airflow e copiar DAGs, se necessário
-  provisioner "file" {
-    source      = "/terraform_factory/task1/dags/" # Diretório local com DAGs
-    destination = "/home/ec2-user/airflow/dags"    # Destino na instância EC2
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("bix_kp.pem")
-      host        = self.public_ip  # evitar referência cíclica
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chown -R ec2-user:ec2-user /home/ec2-user/airflow/dags",
-      "nohup airflow webserver --port 8080 &",
-      "nohup airflow scheduler &"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("bix_kp.pem")
-      host        = self.public_ip  # evitar referência cíclica
-    }
-  }
+# Atribuir um IP elástico à instância
+resource "aws_eip" "public_ip" {
+  instance = aws_instance.airflow_instance.id  # Corrigido para referenciar a instância correta
+  vpc      = true
 }
--- task1/terraform.tfvars
+++ task1/terraform.tfvars
@@ -1,3 +1,3 @@
-instance_type = "t2.micro"
+instance_type = "t2.small"
 ami_id = "ami-0ea3c35c5c3284d82"
 key_name = "bix_kp"
